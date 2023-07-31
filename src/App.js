38b970c1d67ba2938373b7b5927f99addcded783
iamrubydevi <ariesabyss@gmail.com>
2023-07-31 23:07:42 +0530

    Add a route for MyProfile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import {
+  BrowserRouter, Route, Routes,
+} from 'react-router-dom';
 import './App.css';
 import Navigation from './components/Navigation';
+import MyProfile from './components/MyProfile';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navigation />
-    </>
+      <div className="container">
+        <Routes>
+          <Route path="/profile" element={<MyProfile />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
