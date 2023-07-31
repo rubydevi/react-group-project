14c7c68b88a57c10c7e8914d65d2a8776b2e4f48
ctokoli <matumasie@gmail.com>
2023-07-31 17:10:12 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/Navigation';
+import RocketsComponent from './components/Rockets';
+import MissionsComponent from './components/Missions';
+import DragonsComponents from './components/Dragons';
+import ProfileComponent from './components/Profile';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navigation />
-    </>
+      <Routes>
+        <Route path="/" element={<RocketsComponent />} />
+        <Route path="/missions" element={<MissionsComponent />} />
+        <Route path="/dragons" element={<DragonsComponents />} />
+        <Route path="/profile" element={<ProfileComponent />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
