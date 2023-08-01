ebe988ddd4e0d7088ec245572864893e7a1c374c
iamrubydevi <ariesabyss@gmail.com>
2023-08-01 16:00:57 +0530

    Fix Linters

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,9 @@
 import Container from 'react-bootstrap/Container';
 
 const ProfileComponent = () => (
-  <Container><h1>Profile Page</h1></Container>
+  <Container>
+    <h1>Profile Page</h1>
+  </Container>
 );
 
 export default ProfileComponent;
