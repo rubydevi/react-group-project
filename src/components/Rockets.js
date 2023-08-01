b42aca596348e3b7fddf6b0a6524ccd004cc8750
iamrubydevi <ariesabyss@gmail.com>
2023-08-01 15:39:53 +0530

    Make sure rockets are fetched and can be displayed

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,43 @@
 import Container from 'react-bootstrap/Container';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../Redux/Rockets/rocketsSlice';
 
-const RocketsComponent = () => (
-  <Container><h1>Rockets Page</h1></Container>
-);
+const RocketsComponent = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets.rockets);
+  const loading = useSelector((state) => state.rockets.loading);
+  const error = useSelector((state) => state.rockets.error);
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        Error:
+        {error}
+      </div>
+    );
+  }
+
+  return (
+    <Container>
+      <h1>Rockets Page</h1>
+      <div>
+        <ul>
+          {rockets.map((rocket) => (
+            <li key={rocket.id}>{rocket.name}</li>
+          ))}
+        </ul>
+      </div>
+    </Container>
+  );
+};
 
 export default RocketsComponent;
