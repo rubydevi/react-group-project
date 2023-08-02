0f208e9e3c2b5d422620577825f9272fb6f106b1
Ayomide <ayomidesunday31@gmail.com>
2023-08-02 17:49:12 +0100

    Dragon api fetch and layout

--- src/components/Dragons.js
+++ src/components/Dragons.js
@@ -1,10 +1,61 @@
-import Container from 'react-bootstrap/Container';
-import DragonApi from '../Redux/Dragons/dragonsApi'
-const DragonsComponents = () => (
-  <Container>
-    <h1>Dragons Page</h1>
-    <DragonApi />
-  </Container>
-);
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchDragons, cancelReservation, reserveDragon } from '../Redux/Dragons/dragonsSlice';
+import style from '../styles/DragonsPage.module.css';
 
-export default DragonsComponents;
+const DragonsPage = () => {
+  const dispatch = useDispatch();
+  const { dragons } = useSelector((store) => store.dragons);
+
+  useEffect(() => {
+    if (dragons.length === 0) {
+      dispatch(fetchDragons());
+    }
+  }, [dispatch, dragons.length]);
+
+  const handleReserve = (id) => {
+    dispatch(reserveDragon(id));
+  };
+
+  const cancelReserve = (id) => {
+    dispatch(cancelReservation(id));
+  };
+
+  return (
+    <section className="dragons">
+      {dragons.map((dragon) => (
+        <div className={style.dragon} key={dragon.id}>
+          <div className={style.dragonImg}>
+            <img src={dragon.flickr_images[0]} alt={dragon.name} />
+          </div>
+          <div className={style.dragonInfo}>
+            <h2>{dragon.name}</h2>
+            <p>
+              {dragon.reserved && <span className={style.reservedText}>Reserved</span>}
+              {dragon.description}
+            </p>
+            {!dragon.reserved ? (
+              <button
+                type="button"
+                onClick={() => handleReserve(dragon.id)}
+                className={style.reserve}
+              >
+                Reserve Dragon
+              </button>
+            ) : (
+              <button
+                type="button"
+                onClick={() => cancelReserve(dragon.id)}
+                className={style.cancel}
+              >
+                Cancel Reservation
+              </button>
+            )}
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default DragonsPage;
